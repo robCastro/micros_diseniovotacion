6481753b6808f779bfdc2e0845c83cb09a091a09
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-11-13 08:21:59 -0600

    urls de micros produccion actualizadas

--- urls.js
+++ urls.js
@@ -1,2 +1,3 @@
+exports.disenio = 'http://diseniovotacion.herokuapp.com/api/diseniovotacion/'
 exports.participantes = 'https://participantes.herokuapp.com/api/registro_participantes/';
 exports.votacion = 'https://microsvotacion.herokuapp.com/api/votacion/';
