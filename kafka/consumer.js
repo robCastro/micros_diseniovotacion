73c7d71a2e3fc5f0c28af94f9aa20af95989ffd4
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-12-04 09:58:06 -0600

    Cambios a ser confirmados: 	modificado:     config/config.json 	modificado:     kafka/consumer.js
    Post votacion en consumer kafka

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -1,10 +1,13 @@
-var Kafka = require("node-rdkafka");
+const Kafka = require("node-rdkafka");
 require('dotenv').config();
+const models = require('../models/index');
 
-console.log(process.env.CLOUDKARAFKA_BROKERS, process.env.CLOUDKARAFKA_USERNAME, process.env.CLOUDKARAFKA_PASSWORD, process.env.CLOUDKARAFKA_TOPIC_PREFIX);
+const Votacion = models.votacion;
 
 var kafkaConf = {
-  "group.id": "cloudkarafka-example",
+  //Info: https://github.com/edenhill/librdkafka/blob/v1.2.2/CONFIGURATION.md
+  "client.id": "disenio",
+  "group.id": "disenio-group",
   "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
   "socket.keepalive.enable": true,
   "security.protocol": "SASL_SSL",
@@ -29,10 +32,26 @@ consumer.on("ready", function(arg) {
   consumer.consume();
 });
 consumer.on("data", function(m) {
-  console.log("calling commit");
-  consumer.commit(m);
   console.log("m: ", m)
   console.log("m.String: ", m.value.toString());
+  if(m.topic === '40khj5pk-Votacion' && m.partition === 0){
+    jsonVotacion = JSON.parse(m.value.toString());
+    Votacion.create({
+      id_tipo_votacion: parseInt(jsonVotacion.tipoVotacion.id_tipo_votacion),
+      id_ordenamiento: parseInt(jsonVotacion.ordenamiento.id_ordenamiento),
+      fecha_inicio_votacion: jsonVotacion.fecha_inicio_votacion,
+      fecha_fin_votacion: jsonVotacion.fecha_fin_votacion,
+      nombre_votacion: jsonVotacion.nombre_votacion,
+      descripcion_votacion: jsonVotacion.descripcion_votacion
+    }).then(votacion => {
+      console.log('Votacion Guardada: ', jsonVotacion);
+      consumer.commit(m);
+    }).catch(err => {
+      console.log('Error en guardado de Votacion: ', err);
+    });
+  }
+  consumer.commit(m);
+  
 });
 consumer.on("disconnected", function(arg) {
   process.exit();
@@ -46,8 +65,8 @@ consumer.on('event.error', function(err) {
 //});
 consumer.connect();
 
-setTimeout(function() {
-  consumer.disconnect();
-}, 300000);
+//setTimeout(function() {
+//  consumer.disconnect();
+//}, 300000);
 
 module.exports = consumer;
