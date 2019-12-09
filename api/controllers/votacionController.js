ef23c24fa90d16b1c3c2a4f684f6be6fdbdeb06a
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-12-09 08:52:26 -0600

    Listo guardado con Kafka. Testeado para fallos en aplicacion y en base de datos  Cambios a ser confirmados: 	modificado:     api/controllers/votacionController.js 	modificado:     kafka/consumer.js

--- api/controllers/votacionController.js
+++ api/controllers/votacionController.js
@@ -69,90 +69,15 @@ exports.get_votaciones = function(req, res){
 	}
 }
 
-exports.post_votacion = function(req, res){
-	console.log(req.body);
-	console.log(req.body.tipoVotacion.id_tipo_votacion);
-	console.log(req.body.ordenamiento.id_ordenamiento);
-	console.log(new Date())
-	if (isNaN(parseInt(req.body.ordenamiento.id_ordenamiento)) || isNaN(parseInt(req.body.tipoVotacion.id_tipo_votacion))){
-		res.status(400).json({msg: 'Usar parametros numericos'});
-	}
-	else{
-		let fechaInicio = new Date(req.body.fecha_inicio_votacion);
-		let fechaFin = new Date(req.body.fecha_fin_votacion);
-		if(fechaInicio instanceof Date && !isNaN(fechaInicio)){
-			if (fechaInicio.getTime() <= (new Date).getTime()){
-				res.status(400).json({msg: 'La fecha de Inicio debe ser mayor a la fecha actual'});
-			}
-			else{
-				if(fechaFin instanceof Date && !isNaN(fechaFin)){
-					Ordenamiento.findOne({
-						where:{id_ordenamiento: parseInt(req.body.ordenamiento.id_ordenamiento)},
-						attributes: ['id_ordenamiento']
-					}).then(ordenamiento => {
-						if (ordenamiento !== null){
-							TipoVotacion.findOne({
-								where: {id_tipo_votacion: parseInt(req.body.tipoVotacion.id_tipo_votacion)},
-								attributes: ['id_tipo_votacion']
-							}).then(tipo => {
-								if (tipo !== null){
-									Votacion.create({
-										id_tipo_votacion: parseInt(req.body.tipoVotacion.id_tipo_votacion),
-										id_ordenamiento: parseInt(req.body.ordenamiento.id_ordenamiento),
-										fecha_inicio_votacion: fechaInicio,
-										fecha_fin_votacion: fechaFin,
-										nombre_votacion: req.body.nombre_votacion,
-										descripcion_votacion: req.body.descripcion_votacion
-									// este then es en caso exitoso
-									}).then(votacion => {
-										// propagando a microS votacion
-										console.log(urls.votacion + "votacion/")
-										Request.post({
-											"headers": { "content-type": "application/json" },
-											"url": urls.votacion + "votacion/",
-											"body": JSON.stringify(votacion)
-										}).on('response', function(response){
-											if(response.statusCode !== 200){
-												res.status(500).json({ msg: "Error guardando la votacion, revisar microS votacion", micro_resp: response });
-												return votacion.destroy({force: true});
-											}
-											else{
-												res.status(200).json(votacion);
-											}
-										}).on('error', function(err){
-											res.status(500).json({ msg: "Error guardando la votacion, revisar microS votacion" });
-											return votacion.destroy({force: true});
-										});
-									}).catch(err => {
-										console.log('Error guardando la votacion: ' + err);
-				    					res.status(500).json({ msg: "Error guardando la votacion" });
-									});
-									
-								}
-								else{
-									res.status(404).json({msg: 'Tipo Votacion no existe'});
-								}
-							}).catch(err => {
-								console.log('Error verificando existencia de tipos de votacion: ' + err);
-				    			res.status(500).json({ msg: "Error verificando existencia de tipos de votacion" });
-							});
-						}
-						else{
-							res.status(404).json({msg: 'Ordenamiento no existe'});
-						}
-					}).catch(err => {
-						console.log('Error verificando existencia de ordenamiento: ' + err);
-				    	res.status(500).json({ msg: "Error verificando existencia de ordenamiento" });
-					});
-				}
-				else{
-					res.status(400).json({msg: 'Fecha de fin no valida'});
-				}
-			}
-		}
-		else{
-			res.status(400).json({msg: 'Fecha de inicio no valida'});
-		}
-	}
-	
+// La funcion retornará la promesa de Crear una votacion
+// El then y el catchh serán manejados en el consumer para hacer o no consumer.commit
+exports.post_votacion = async function(jsonVotacion){
+	return await Votacion.create({
+		id_tipo_votacion: parseInt(jsonVotacion.tipoVotacion.id_tipo_votacion),
+		id_ordenamiento: parseInt(jsonVotacion.ordenamiento.id_ordenamiento),
+		fecha_inicio_votacion: jsonVotacion.fecha_inicio_votacion,
+		fecha_fin_votacion: jsonVotacion.fecha_fin_votacion,
+		nombre_votacion: jsonVotacion.nombre_votacion,
+		descripcion_votacion: jsonVotacion.descripcion_votacion
+	});
 }
--- kafka/consumer.js
+++ kafka/consumer.js
@@ -1,26 +1,27 @@
 const Kafka = require("node-rdkafka");
 require('dotenv').config();
-const models = require('../models/index');
 
-const Votacion = models.votacion;
+const post_votacion = require('../api/controllers/votacionController').post_votacion;
 
 var kafkaConf = {
   //Info: https://github.com/edenhill/librdkafka/blob/v1.2.2/CONFIGURATION.md
-  "client.id": "disenio",
   "group.id": "disenio-group",
+  "client.id": "disenio",
   "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
   "socket.keepalive.enable": true,
   "security.protocol": "SASL_SSL",
   "sasl.mechanisms": "SCRAM-SHA-256",
   "sasl.username": process.env.CLOUDKARAFKA_USERNAME,
   "sasl.password": process.env.CLOUDKARAFKA_PASSWORD,
-  "debug": "generic,broker,security"
+  "debug": "generic,broker,security",
+  "enable.auto.commit": false
 };
 
 const prefix = process.env.CLOUDKARAFKA_TOPIC_PREFIX;
 const topics = [`${prefix}Votacion`];
 const consumer = new Kafka.KafkaConsumer(kafkaConf, {
-  "auto.offset.reset": "beginning"
+  "auto.offset.reset": "beginning",
+
 });
 consumer.on("error", function(err) {
   console.error(err);
@@ -32,27 +33,25 @@ consumer.on("ready", function(arg) {
   consumer.consume();
 });
 consumer.on("data", function(m) {
-  console.log("m: ", m)
-  console.log("m.String: ", m.value.toString());
   if(m.topic === '40khj5pk-Votacion' && m.partition === 0){
     jsonVotacion = JSON.parse(m.value.toString());
-    Votacion.create({
-      id_tipo_votacion: parseInt(jsonVotacion.tipoVotacion.id_tipo_votacion),
-      id_ordenamiento: parseInt(jsonVotacion.ordenamiento.id_ordenamiento),
-      fecha_inicio_votacion: jsonVotacion.fecha_inicio_votacion,
-      fecha_fin_votacion: jsonVotacion.fecha_fin_votacion,
-      nombre_votacion: jsonVotacion.nombre_votacion,
-      descripcion_votacion: jsonVotacion.descripcion_votacion
-    }).then(votacion => {
-      console.log('Votacion Guardada: ', jsonVotacion);
-      consumer.commit(m);
+    let promesa = post_votacion(jsonVotacion);
+    // Lo retornado es una promesa
+    console.log('Retorno Promesa: ', promesa);
+    promesa.then(votacion => {
+      consumer.commitMessage(m);
+      console.log('Commit hecho', m);
     }).catch(err => {
-      console.log('Error en guardado de Votacion: ', err);
+      // Si hay un error, no se hace commit del mensaje
+      // Probar, por ejemplo, a parar por un segundo el proceso de PostgreSQL
+      // Enviar mensajes usando el producer y
+      // Luego subir de nuevo postgres y reiniciar el consumer
+      console.log('Error: ', err);
     });
   }
-  consumer.commit(m);
-  
 });
+
+
 consumer.on("disconnected", function(arg) {
   process.exit();
 });
@@ -60,13 +59,7 @@ consumer.on('event.error', function(err) {
   console.error(err);
   process.exit(1);
 });
-//consumer.on('event.log', function(log) {
-//  console.log(log);
-//});
-consumer.connect();
 
-//setTimeout(function() {
-//  consumer.disconnect();
-//}, 300000);
+consumer.connect();
 
 module.exports = consumer;
